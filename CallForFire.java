4dca39760cfb42d17c7d3faf774957f17ce0946b
Pete1230z <petree234@gmail.com>
2024-04-17 21:06:51 +0900

    Converting Int to String

--- CallForFire.java
+++ CallForFire.java
@@ -6,20 +6,15 @@ import java.util.Scanner;
 
 //Class that creates CallForFire
 public class CallForFire {
+	private int UserGrid;
 	private String MGRS;
 	private String grid;
     
 	//Constructor that sets the CallForFire parameters
-	public CallForFire(String MGRS, String grid) {
-		this.MGRS = MGRS;
+	public CallForFire(int UserGrid, String MGRS, String grid) {
+		this.UserGrid = UserGrid;
+		this.MGRS = String.valueOf(UserGrid);
 		this.grid = grid;
-	}
-
-	// Sets the value of attribute MGRS to value passed as parameter MGRS
-	public void setMGRS(String MGRS) {
-		if(MGRS.length() >= 6) {
-			this.MGRS = MGRS;
-		}
 	}
 
 	public String getMGRS() {
