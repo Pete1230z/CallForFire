718c2feb67912e81a5f672c18273fd508c6a830b
Pete1230z <petree234@gmail.com>
2024-04-18 19:05:16 +0900

    Can call in a mission successfully

--- CFFCompiler.java
+++ CFFCompiler.java
@@ -5,12 +5,26 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CFFCompiler {
+	static int choice;
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter your position in MGRS Grid with your Grid Zone Designator: ");
 		String MGRS = input.nextLine();
-		System.out.print("Your position in MGRS Grid is" + " " + getMGRSFormat(MGRS));
-	}	
+		System.out.println("Your position in MGRS Grid is" + " " + getMGRSFormat(MGRS));
+		System.out.println("What is your method of target location?");
+		System.out.println("1. Grid\n2. Polar\n3. Shift");
+		int choice = input.nextInt();
+		if (choice == 1) {
+			System.out.println("Your are conducting a Grid mission");
+		} else if(choice == 2) {
+			System.out.println("Your are conducting a Grid mission");
+		} else if(choice == 3){
+			System.out.println("Your are conducting a Grid mission");
+		} else {
+			System.out.println("That is not an option.");
+		}
+	}
 	
     //Converts MGRS Grid into a proper format
 	public static String getMGRSFormat(String MGRS) {
